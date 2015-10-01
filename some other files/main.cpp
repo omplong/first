013310c47b54970db89b983a58f9c2370f959b80
omplong <gamayotok@gmail.com>
2015-10-01 12:40:55 +0800

    [sample file] main.cpp
    just to upload something. a branch will be created from this

---
+++ some other files/main.cpp
@@ -0,0 +1,34 @@
+//============================================================================
+// Name        : jmayol.cpp
+// Author      : Jeff Mayol
+// Version     :
+// Copyright   : Your copyright notice
+// Description : call the exercises you want here
+//============================================================================
+
+#include <iostream>
+#include "main_api.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+/*
+ 'A' - this is a character
+ "A" - this is a string
+
+ * */
+//char stri[] = "oh no is it oh?";
+
+
+int main (int argc, char **argv)
+{
+	//grading_exercise();
+	//guess_my_random_number();
+	//brackets();
+	//names();
+	tictic();
+
+    return 0;
+
+}
